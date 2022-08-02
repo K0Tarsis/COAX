90af9d03bd0b40dbba2deeb0906b8670582213dc
Kostiantyn <kostiantyn.hovorukha@gmail.com>
2022-08-02 09:46:14 +0300

    Second

--- second.py
+++ second.py
@@ -3,9 +3,6 @@ from moviepy.editor import VideoFileClip
 import os
 
 
-link = "https://v16-webapp.tiktok.com/b6135b375383040ef6c1045b41bcffcc/62e8723f/video/tos/useast2a/tos-useast2a-ve-0068c004/68ba42ffc5674ce6803cd729c00a39e3/?a=1988&ch=0&cr=0&dr=0&lr=tiktok_m&cd=0%7C0%7C1%7C0&cv=1&br=3402&bt=1701&btag=80000&cs=0&ds=3&ft=gKSYZ8hNo0PD1Wh44sg9wSX2O5LiaQ2D~AT&mime_type=video_mp4&qs=0&rc=NDw5MzpoaTg1NjNlODc6NkBpM2h0ZWY6ZndsZDMzNzczM0BjMTFgXzBfXzExLl4vX2EvYSNpazVgcjRfMG1gLS1kMTZzcw%3D%3D&l=202208011838380102171351372454704E"
-
-
 def download(link:str):
     r = requests.get(link, stream = True)
 
@@ -21,6 +18,7 @@ def convert():
 
 
 if __name__ == "__main__":
-    download(input())
+    link = input()
+    download(link)
     convert()
     print(os.path.abspath("TikTok.gif"))
